b28e942581a9d604195574e0d25bfc47420c7d37
Yeojin Kim <yeojin58@naver.com>
2023-11-14 16:48:07 +0900

    feat: 사용자 메뉴 입력 예외 발생

--- src/InputView.js
+++ src/InputView.js
@@ -1,4 +1,5 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
+import menuPrices from "./MenuPrices";
 
 const InputView = {
   async readDate() {
@@ -6,6 +7,7 @@ const InputView = {
     do {
       const input = await MissionUtils.Console.readLineAsync("12월 중 식당 예정 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
       date = parseInt(input, 10);
+
       if (isNaN(date) || date < 1 || date > 31) {
         MissionUtils.Console.print("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
       }
@@ -19,23 +21,35 @@ const InputView = {
       const input = await MissionUtils.Console.readLineAsync("주문할 메뉴를 입력해 주세요 (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
       order = this.parseOrderInput(input);
 
-      if (!this.isValidOrder(order)) {
+      if (!order || !this.isValidOrder(order)) {
         MissionUtils.Console.print("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
       }
-    } while (!this.isValidOrder(order));
+    } while (!order || !this.isValidOrder(order));
 
     return order;
   },
 
   parseOrderInput(input) {
-    return input.split(',').map(item => {
-      const [name, quantity] = item.split('-');
+    const parsedOrder =  input.split(',').map(item => {
+      const parts = item.split('-');
+      if (parseInt.length !== 2) {
+        return null;
+      }
+      const [name, quantity] = parts;
       return { name, quantity: parseInt(quantity, 10) };
     });
+    return parsedOrder.some(item => item === null) ? null : parsedOrder;
   },
 
   isValidOrder(order) {
-    return order.every(item => item.name && !isNaN(item.quantity));
+    const menuNames = order.map(item => item.name);
+    const uniqueMenuNames = new Set(menuNames);
+    if (uniqueMenuNames.size !== menuNames.length) {
+      return false;
+    }
+
+    return order.every(item => 
+      item.name && !isNaN(item.quantity) && item.quantity >=1 && menuPrices.hasOwnProperty(item.name));
   }
 };
 
