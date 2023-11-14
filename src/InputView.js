1eed87f04ab1f3c77b9a270dfae9815bc0cdb146
Yeojin Kim <yeojin58@naver.com>
2023-11-14 16:23:29 +0900

    refactor: InputView.js

--- src/InputView.js
+++ src/InputView.js
@@ -15,21 +15,27 @@ const InputView = {
 
   async readOrder() {
     let order;
-    let isValidOrder = false;
     do {
       const input = await MissionUtils.Console.readLineAsync("주문할 메뉴를 입력해 주세요 (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
-      order = input.split(',').map(item => {
-        const [name, quantity] = item.split('-');
-        return { name, quantity: parseInt(quantity, 10) };
-      });
+      order = this.parseOrderInput(input);
 
-      isValidOrder = order.every(item => item.name && !isNaN(item.quantity));
-      if (!isValidOrder) {
+      if (!this.isValidOrder(order)) {
         MissionUtils.Console.print("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
       }
-    } while (!isValidOrder);
+    } while (!this.isValidOrder(order));
 
     return order;
+  },
+
+  parseOrderInput(input) {
+    return input.split(',').map(item => {
+      const [name, quantity] = item.split('-');
+      return { name, quantity: parseInt(quantity, 10) };
+    });
+  },
+
+  isValidOrder(order) {
+    return order.every(item => item.name && !isNaN(item.quantity));
   }
 };
 
