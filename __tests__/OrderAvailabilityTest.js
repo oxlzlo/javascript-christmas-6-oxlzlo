e0acd1d7c10042eeef307c672a5d8f27ad356b0a
Yeojin Kim <yeojin58@naver.com>
2023-11-15 06:08:21 +0900

    test: 메뉴 입력 예외 테스트 추가 구현

--- __tests__/OrderAvailabilityTest.js
+++ __tests__/OrderAvailabilityTest.js
@@ -1,23 +1,60 @@
 import App from "../src/App";
+import InputView from "../src/InputView";
+
+jest.mock('../src/InputView.js', () => ({
+    readOrder: jest.fn()
+}));
 
 describe ('App', () => {
     describe('isValidOrder', () => {
-        it ('음료만 주문 할 경우 주문 불가능', () => {
+        it ('음료만 주문 할 경우 주문 불가능', async () => {
             const app = new App();
 
-            const order = [{ name: "제로콜라", quantity: 1 }];
-            const isValid = app.inputView.isValidOrder(order);
+            const inputOrder = "제로콜라-1"
+            InputView.readOrder.mockResolvedValue("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
 
-            expect(isValid).toBe(false);
+            const order = await app.inputView.readOrder(inputOrder);
+            expect(order).toBe("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
         });
 
-        it ('주문 메뉴는 최대 20개까지만 가능', () => {
+        it ('주문 메뉴는 최대 20개까지만 가능', async () => {
             const app = new App();
 
-            const order = [{ name: "양송이수프", quantity: 21 }];
-            const isValid = app.inputView.isValidOrder(order);
+            const inputOrder = "양송이수프-21"
+            InputView.readOrder.mockResolvedValue("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
 
-            expect(isValid).toBe(false);
+            const order = await app.inputView.readOrder(inputOrder);
+            expect(order).toBe("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        });
+
+        it ('메뉴판에 없는 메뉴 주문 시 에러 발생', async () => {
+            const app = new App();
+
+            const inputOrder = "신호등치킨-1"
+            InputView.readOrder.mockResolvedValue("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+
+            const order = await app.inputView.readOrder(inputOrder);
+            expect(order).toBe("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        });
+
+        it ('주문 메뉴 형식이 예시와 다른 경우 에러 발생', async () => {
+            const app = new App();
+
+            const inputOrder = "바비큐립2";
+            InputView.readOrder.mockResolvedValue("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+
+            const order = await app.inputView.readOrder(inputOrder);
+            expect(order).toBe("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        });
+
+        it ('중복 메뉴를 입력한 경우 에러 발생', async () => {
+            const app = new App();
+
+            const inputOrder = ["시저샐러드-1", "시저샐러드-2"];
+            InputView.readOrder.mockResolvedValue("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+
+            const order = await app.inputView.readOrder(inputOrder);
+            expect(order).toBe("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
         });
     }); 
 });
