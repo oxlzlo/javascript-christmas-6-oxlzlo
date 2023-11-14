0bc990bba5f9ab060c9b00299d2103d3f695ab86
Yeojin Kim <yeojin58@naver.com>
2023-11-15 04:58:35 +0900

    test: 방문 날짜 입력시 에러 발생 케이스 테스트 구현

---
+++ __tests__/VisitDateTest.js
@@ -0,0 +1,30 @@
+import App from "../src/App";
+import InputView from "../src/InputView";
+
+jest.mock('../src/InputView.js', () => ({
+    readDate: jest.fn()
+}));
+
+describe ('App', () => {
+    describe ('isValidVisitDate', () => {
+        it ('방문 날짜는 숫자만 입력 가능하다', async () =>{
+            const app = new App();
+
+            const inputDate = "4일";
+            InputView.readDate.mockResolvedValue("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+
+            const date = await app.inputView.readDate(inputDate);          
+            expect(date).toBe("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        });
+
+        it ('방문 날짜는 1 이상, 31 이하의 숫자만 입력 가능하다', async () => {
+            const app = new App();
+
+            const inputDate = 32;
+            InputView.readDate.mockResolvedValue("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+
+            const date = await app.inputView.readDate(inputDate);
+            expect(date).toBe("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        });
+    });
+});
